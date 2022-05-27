fe2217d8129258c36f78868b534b7f1b685ea222
Alexander Byun <alexanderbyun@AB-MBP16.attlocal.net>
2022-05-27 16:37:40 -0400

    backend test run

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const mongoose = require ('mongoose');
 const db = mongoose.connection;
 
 // Connect to Mongo
-mongoose.connect(MONGODB_URI  ,  { useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, () => {
+    console.log('whatever')
+})
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
