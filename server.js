c3aa51680e8a79b2b02d2c64ffc03d997b51202f
Alexander Byun <alexanderbyun@AB-MBP16.attlocal.net>
2022-05-31 10:34:54 -0400

    local access

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-const MONGODB_URI  = process.env.MONGODB_URI
+
 const mongoose = require ('mongoose');
 const db = mongoose.connection;
 const express = require ('express');
 const app = express ();
 
-
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3003;
+// const MONGODB_URI  = process.env.MONGODB_URI
+const mongoURI = "mongodb://localhost:27017/"
 
-require("dotenv").config()
+// Connect to Mongo
+// mongoose.connect(MONGODB_URI, () => {
+//     console.log('Mongo live')
+// })
+mongoose.connect(mongoURI, () => {
+  console.log('Mongo live')
+});
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
   })
 
-// Connect to Mongo
-mongoose.connect(MONGODB_URI, () => {
-    console.log('whatever')
-})
+
+
 
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
