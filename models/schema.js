d1aead41151f7b5150dce3d723e822e4ddacd886
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-02 10:09:08 -0700

    ammended schema for new website key

--- models/schema.js
+++ models/schema.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    type: String,
-    chef: String,
-    image: String,
-    rank: {type: Number, unique: true, message: 'Rank must be Unique'},
-    show: Boolean,
+	name: String,
+	address: String,
+	type: String,
+	chef: String,
+	image: String,
+	rank: {
+		type: Number,
+		unique: true,
+	},
+	show: Boolean,
+	website: String
 })
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const db = mongoose.connection;
-const express = require ('express');
-const app = express ();
+const express = require('express');
+const app = express();
 const cors = require('cors');
 const Restaurant = require('./models/schema.js')
 
@@ -11,46 +11,48 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.post('/restaurants',(req, res) => {
-  Restaurant.create(req.body, (err, createdRestaurant) => {
-      res.json(createdRestaurant)
-      console.log(err);
-  })
+app.post('/restaurants', (req, res) => {
+	Restaurant.create(req.body, (err, createdRestaurant) => {
+		res.json(createdRestaurant)
+	})
 })
 
 app.get('/restaurants', (req, res) => {
-  Restaurant.find({
-  }).collation({'locale':'en'}).sort('rank').exec((err, foundRestaurant) => {
-      res.json(foundRestaurant)
-  })
+	Restaurant.find({}).collation({
+		'locale': 'en'
+	}).sort('rank').exec((err, foundRestaurant) => {
+		res.json(foundRestaurant)
+	})
 })
 
 app.delete('/restaurants/:id', (req, res) => {
-  Restaurant.findByIdAndRemove(req.params.id, (err, deletedResaurant) => {
-      res.json(deletedResaurant)
-  })
+	Restaurant.findByIdAndRemove(req.params.id, (err, deletedResaurant) => {
+		res.json(deletedResaurant)
+	})
 })
 
 app.put('/restaurants/:id', (req, res) => {
-  Restaurant.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedRestaurant) => {
-        res.json(updatedRestaurant)
-    })
+	Restaurant.findByIdAndUpdate(req.params.id, req.body, {
+		new: true
+	}, (err, updatedRestaurant) => {
+		res.json(updatedRestaurant)
+	})
 })
 
 const PORT = process.env.PORT || 3003;
-const MONGODB_URI  = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 const mongoURI = "mongodb://localhost:27017/"
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI, () => {
-    console.log('Mongo live')
+	console.log('Mongo live')
 })
 // mongoose.connect(mongoURI, () => {
 //   console.log('Mongo live')
 // });
 
 // Listener
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+app.listen(PORT, () => console.log('Listening on port:', PORT));
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
